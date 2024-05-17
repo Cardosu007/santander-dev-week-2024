b4a405314a7fbad24c5d16a449cd8e3a3da36468
W Cardoso <cardosu7@gmail.com>
2024-05-17 11:33:40 -0300

    adicionando service

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -9,5 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    boolean existsByAccountNumber(String accountNumber);
+
 
 }
